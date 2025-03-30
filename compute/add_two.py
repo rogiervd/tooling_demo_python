2237a90761e0545800d54d28a5772af4940d0fe7
Rogier van Dalen <rogiervd@gmail.com>
2025-03-30 11:45:25 +0100

    Introduce documentation building with Sphinx under Bazel

--- compute/add_two.py
+++ compute/add_two.py
@@ -4,8 +4,19 @@ Class that allows one to add 2, modulo some value.
 
 
 class AddTwo:
+    """
+    Function object that adds 2, but returns the remainder after using modulus
+    ``modulus``.
+
+    :param modulus: The modulus to use for the computation.
+    """
+
     def __init__(self, modulus):
         self.modulus = modulus
 
     def __call__(self, value):
+        """
+        :return:
+            ``value``, but with 2 added, modulo ``self.modulus``.
+        """
         return (value + 2) % self.modulus
---
+++ doc/index.rst
@@ -0,0 +1,11 @@
+Tooling demo - Python
+=====================
+
+This is a demo of tooling for Python.
+
+.. toctree::
+    :maxdepth: 2
+
+    reference/api.rst
+
+.. Add other files to be included here.
